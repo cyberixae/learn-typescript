6ae312433c04c6107a434db0a669a678ea9d8663
Toni Ruottu <toni.ruottu@iki.fi>
2020-07-04 18:53:51 +0300

    Add exercise 1

---
+++ src/exercise1.ts
@@ -0,0 +1,49 @@
+
+// Recommendation: Use TypeScript 4
+
+import type { Zero, Succ, Prec, Plus, Minus } from './spoiler';
+import { str, zero, succ, prec, plus, minus } from './spoiler';
+
+type One = Succ<Zero>
+type Two = Succ<One>
+type Three = Succ<Two>
+type Four = Succ<Three>
+type Five = Succ<Four>
+type Six = Succ<Five>
+type Seven = Succ<Six>
+type Eight = Succ<Seven>
+type Nine = Succ<Eight>
+
+const one: One = succ(zero)
+const two: Two = succ(one)
+const three: Three = succ(two)
+const four: Four = succ(three)
+const five: Five = succ(four)
+const six: Six = succ(five)
+const seven: Seven = succ(six)
+const eight: Eight = succ(seven)
+const nine: Nine = succ(eight)
+
+const testZeroI: Exclude<Zero, One> = zero
+const testZeroE: Exclude<One, Zero> = one
+
+const testFourI: Exclude<Four, Two> = four
+const testFourE: Exclude<Two, Four> = two
+
+const testSuccR: Five = succ(four)
+const testSuccT: Succ<Four> = five
+
+const testPrecR: Two = prec(three)
+const testPrecT: Prec<Three> = two
+
+const testPlusR: Nine = plus(four, five)
+const testPlusT: Plus<Four, Five> = nine
+
+const testMinusR: Three = minus(five, two)
+const testMinusT: Minus<Five, Two> = three
+
+console.log([testZeroI, testZeroE, testFourI, testFourE].map(str)) // [0, 1, 4, 2]
+console.log([testSuccT, testPrecT, testPlusT, testMinusT].map(str)) // [5, 2, 9, 3]
+console.log([testSuccR, testPrecR, testPlusR, testMinusR].map(str)) // [5, 2, 9, 3]
+
+export default {}
